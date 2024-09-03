ca7b9a1f13feee47de28ed556d3dfab7f0188a87
Luis <luiscrsarmento@gmail.com>
2024-09-03 14:00:18 -0300

    Primeiro commit

---
+++ Back-end/server.js
@@ -0,0 +1,60 @@
+import express, { query } from 'express'
+import cors from 'cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+app.post('/tasks', async (req, res) => {
+
+    await prisma.task.create({
+        data: {
+            tittle: req.body.tittle,
+            description: req.body.description,
+            status: req.body.status
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.get('/tasks', async (req, res) => {
+    let tasks = []
+    
+    tasks = await prisma.task.findMany()
+
+    res.status(200).json(tasks)
+})
+
+app.put('/tasks/:id', async (req, res) => {
+
+    await prisma.task.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            tittle: req.body.tittle,
+            description: req.body.description,
+            status: req.body.status
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+
+    await prisma.task.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({ message: 'Usuário deletado com sucesso!' })
+})
+
+app.listen(3000)
---
+++ Front-end/front-end/src/Pages/Home/index.jsx
@@ -0,0 +1,89 @@
+import { useEffect, useState, useRef } from 'react'
+import './style.css'
+import Trash from '../../assets/trash.png'
+import api from '../../services/api'
+
+function Home() {
+  const [tasks, setTasks] = useState([])
+
+  const inputTitlle = useRef()
+  const inputDescription = useRef()
+
+  async function getTasks() {
+    const tasksFromApi = await api.get('/tasks')
+    setTasks(tasksFromApi.data)
+  }
+
+  async function createTasks() {
+
+    await api.post('/tasks', {
+      tittle: inputTitlle.current.value,
+      description: inputDescription.current.value,
+      status: false
+    })
+    inputTitlle.current.value = ''
+    inputDescription.current.value = ''
+    getTasks()
+  }
+
+  async function deleteTasks(id) {
+    await api.delete(`/tasks/${id}`)
+    getTasks()
+  }
+
+  useEffect(() => {
+    getTasks()
+  }, [])
+
+  async function toggleTaskCompletion(event, task) {
+    const cardElement = event.target.closest('.card');
+
+    const newStatus = event.target.checked;
+
+    if (newStatus) {
+      cardElement.classList.add('completed');
+    } else {
+      cardElement.classList.remove('completed');
+    }
+
+    await api.put(`/tasks/${task.id}`, {
+      status: newStatus,
+    });
+
+    getTasks();
+  }
+
+  return (
+    <div className='container'>
+      <form>
+        <h1>Lista de tarefas</h1>
+        <input placeholder='Tarefa' name='tittle' type='text' ref={inputTitlle} />
+        <input placeholder='Descrição' name='description' type='text' ref={inputDescription} />
+        <button type='button' onClick={createTasks}>Registrar</button>
+      </form>
+
+      {tasks.map(task => (
+
+        <div className={`card ${task.status ? 'completed' : ''}`} key={task.id}>
+          <input
+            type="checkbox"
+            className="complete-checkbox"
+            onChange={(event) => toggleTaskCompletion(event, task)}
+            checked={task.status}
+          />
+          <div>
+            <p><span>{task.tittle}</span></p>
+            <p>{task.description}</p>
+          </div>
+          <button onClick={() => deleteTasks(task.id)}>
+            <img src={Trash} />
+          </button>
+        </div>
+
+      ))}
+
+    </div>
+  )
+}
+
+export default Home
